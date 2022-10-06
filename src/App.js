e775792bffa344a6861daf407628e283bdc86af5
Erdinch <erdinchorhan02@gmail.com>
2022-10-06 19:01:49 +0300

    main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import levelOne from './assets/images/2687204.jpg';
+import levelTwo from './assets/images/2687208.jpg';
+import levelThree from './assets/images/2687215.jpg';
+import './styles/global.css';
+import './styles/app.css';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="select-level-container">
+      <div className="level-one">
+        <Link to="/level-one">
+          <img alt="level one" src={levelOne} />
+        </Link>
+      </div>
+      <div className="level-two">
+        <Link to="level-two">
+          <img alt="level two" src={levelTwo} />
+        </Link>
+      </div>
+      <div className="level-three">
+        <Link to="level-three">
+          <img alt="level three" src={levelThree} />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default App;
