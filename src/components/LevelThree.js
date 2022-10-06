f671500d289601186e4f601f1155055fe4d0692e
Erdinch <erdinchorhan02@gmail.com>
2022-10-06 19:01:17 +0300

    levels // different pictures

---
+++ src/components/LevelThree.js
@@ -0,0 +1,47 @@
+import '../styles/level-one.css';
+import image from '../assets/images/2687215.jpg';
+import React from 'react';
+import WithMagnifier from './WithMagnifier';
+import BackButton from './BackButton';
+import Timer from './Timer';
+import Magnifier from './Magnifier';
+
+const LevelThree = (props) => {
+  const {
+    handleMouseEnter,
+    handleMouseMove,
+    x,
+    y,
+    imgWidth,
+    imgHeight,
+    showMagnifier,
+    handleMouseLeave,
+  } = props;
+
+  return (
+    <div className="main-container">
+      <div className="image-container">
+        <BackButton />
+        <Timer />
+        <img
+          alt="where is waldo level one"
+          src={image}
+          onMouseMove={handleMouseMove}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        />
+        {showMagnifier && (
+          <Magnifier
+            x={x}
+            y={y}
+            image={image}
+            imgWidth={imgWidth}
+            imgHeight={imgHeight}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default WithMagnifier(LevelThree);
