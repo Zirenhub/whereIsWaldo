660ee10cbf3029a34108a0173eddf054b6d4bbad
Erdinch <erdinchorhan02@gmail.com>
2022-10-07 22:30:33 +0300

    marker position indicator

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,14 +1,14 @@
 import '../styles/marker.css';
 
 const Marker = (props) => {
-  const { marX, marY } = props;
+  const { marX, marY, position } = props;
 
   return (
     <div
       className="marker-container"
       style={{ left: marX + '%', top: marY + '%' }}
     >
-      <p>X</p>
+      <p>{position}</p>
     </div>
   );
 };
