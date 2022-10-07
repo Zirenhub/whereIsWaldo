4d5be7c9a04f4881a277a4b89c43939663095301
Erdinch <erdinchorhan02@gmail.com>
2022-10-07 22:30:24 +0300

    waldo position validation

--- src/components/LevelOne.js
+++ src/components/LevelOne.js
@@ -1,11 +1,12 @@
 import '../styles/level-one.css';
 import image from '../assets/images/2687204.jpg';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import WithMagnifier from './WithMagnifier';
 import BackButton from './BackButton';
 import Timer from './Timer';
 import Magnifier from './Magnifier';
 import Marker from './Marker';
+import GameOver from './GameOver';
 
 const LevelOne = (props) => {
   const {
@@ -17,16 +18,55 @@ const LevelOne = (props) => {
     imgHeight,
     showMagnifier,
     handleMouseLeave,
-    handleClick,
-    marX,
-    marY,
   } = props;
+
+  const [[marX, marY], setMarker] = useState([0, 0]);
+  const [isFirstWaldoFound, setIsFirstWaldoFound] = useState(false);
+  const [areAllWaldosFound, setAreAllWaldosFound] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [modalShow, setModalShow] = useState(false);
+
+  const handleCloseModal = () => setModalShow(false);
+  const handleShowModal = () => setModalShow(true);
+
+  const handleClick = (e) => {
+    const imgRect = e.currentTarget.getBoundingClientRect();
+    const { clientX, clientY } = e;
+
+    const posXpct = ((clientX - imgRect.x) * 100) / imgRect.width;
+    const posYpct = ((clientY - imgRect.y) * 100) / imgRect.height;
+
+    setMarker([posXpct, posYpct]);
+  };
+
+  useEffect(() => {
+    const firstWaldoPosition = [75.768823, 76.286825];
+    if (!isFirstWaldoFound) {
+      if (
+        (marY === firstWaldoPosition[0] ||
+          (marY >= 74.48219 && marY <= 77.025085)) &&
+        (marX === firstWaldoPosition[1] ||
+          (marX <= 76.829268 && marY >= 74.549311))
+      ) {
+        setIsFirstWaldoFound(true);
+        // handleShowModal();
+      }
+    }
+
+    // return () => {
+    //   setIsFirstWaldoFound(false);
+    // };
+  }, [marX, marY]);
 
   return (
     <div className="main-container">
       <div className="image-container">
         <BackButton />
-        <Timer />
+        <Timer
+          isWaldoFound={areAllWaldosFound}
+          setSeconds={setSeconds}
+          seconds={seconds}
+        />
         <img
           alt="where is waldo level one"
           src={image}
@@ -44,7 +84,11 @@ const LevelOne = (props) => {
             imgHeight={imgHeight}
           />
         )}
-        <Marker marX={marX} marY={marY} />
+        {isFirstWaldoFound && <Marker marX={marX} marY={marY} position={1} />}
+
+        {/* {modalShow && (
+          <GameOver time={seconds} handleCloseModal={handleCloseModal} />
+        )} */}
       </div>
     </div>
   );
--- src/components/LevelTwo.js
+++ src/components/LevelTwo.js
@@ -1,11 +1,12 @@
 import '../styles/level-one.css';
 import image from '../assets/images/2687208.jpg';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import WithMagnifier from './WithMagnifier';
 import BackButton from './BackButton';
 import Timer from './Timer';
 import Magnifier from './Magnifier';
 import Marker from './Marker';
+import GameOver from './GameOver';
 
 const LevelTwo = (props) => {
   const {
@@ -17,18 +18,53 @@ const LevelTwo = (props) => {
     imgHeight,
     showMagnifier,
     handleMouseLeave,
-    handleClick,
-    marX,
-    marY,
   } = props;
+
+  const [[marX, marY], setMarker] = useState([0, 0]);
+  const [isWaldoFound, setIsWaldoFound] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [modalShow, setModalShow] = useState(false);
+
+  const handleCloseModal = () => setModalShow(false);
+  const handleShowModal = () => setModalShow(true);
+
+  const handleClick = (e) => {
+    const imgRect = e.currentTarget.getBoundingClientRect();
+    const { clientX, clientY } = e;
+
+    const posXpct = ((clientX - imgRect.x) * 100) / imgRect.width;
+    const posYpct = ((clientY - imgRect.y) * 100) / imgRect.height;
+
+    setMarker([posXpct, posYpct]);
+  };
+
+  useEffect(() => {
+    const waldoPosition = [75.768823, 76.286825];
+
+    if (
+      (marY === waldoPosition[0] || (marY >= 74.48219 && marY <= 77.025085)) &&
+      (marX === waldoPosition[1] || (marX <= 76.829268 && marY >= 74.549311))
+    ) {
+      setIsWaldoFound(true);
+      handleShowModal();
+    }
+
+    return () => {
+      setIsWaldoFound(false);
+    };
+  }, [marX, marY]);
 
   return (
     <div className="main-container">
       <div className="image-container">
         <BackButton />
-        <Timer />
+        <Timer
+          isWaldoFound={isWaldoFound}
+          setSeconds={setSeconds}
+          seconds={seconds}
+        />
         <img
-          alt="where is waldo level one"
+          alt="where is waldo level two"
           src={image}
           onMouseMove={handleMouseMove}
           onMouseEnter={handleMouseEnter}
@@ -45,6 +81,9 @@ const LevelTwo = (props) => {
           />
         )}
         <Marker marX={marX} marY={marY} />
+        {modalShow && (
+          <GameOver time={seconds} handleCloseModal={handleCloseModal} />
+        )}
       </div>
     </div>
   );
