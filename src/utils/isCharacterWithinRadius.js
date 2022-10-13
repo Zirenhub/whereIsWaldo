33290d0e1265cf3c746f749cc56746691d52db06
Erdinch <erdinchorhan02@gmail.com>
2022-10-13 14:34:51 +0300

    search radius changed

--- src/utils/isCharacterWithinRadius.js
+++ src/utils/isCharacterWithinRadius.js
@@ -1,10 +1,10 @@
 const isCharacterWithinRadius = (x, y, charX, charY) => {
   const tempX = parseFloat(x.toFixed(6));
   const tempY = parseFloat(y.toFixed(6));
-  const radiusXPlus = charX + 1;
-  const radiusXMinus = charX - 1;
-  const radiusYPlus = charY + 1.7;
-  const radiusYMinus = charY - 1.7;
+  const radiusXPlus = charX + 1.4;
+  const radiusXMinus = charX - 1.4;
+  const radiusYPlus = charY + 1;
+  const radiusYMinus = charY - 1;
 
   if (
     (tempY === charY || (tempY >= radiusYMinus && tempY <= radiusYPlus)) &&
