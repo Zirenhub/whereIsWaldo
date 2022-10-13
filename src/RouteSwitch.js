89dca6c787b5951ce2b474b6f505038245d1e9b3
Erdinch <erdinchorhan02@gmail.com>
2022-10-13 20:56:37 +0300

    google auth and context

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -3,16 +3,23 @@ import App from './App';
 import LevelOne from './components/LevelOne';
 import LevelTwo from './components/LevelTwo';
 import LevelThree from './components/LevelThree';
+import { AuthContextProvider } from './context/AuthContext';
+import SignIn from './components/SignIn';
+import Dropdown from './components/Dropdown';
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/level-one" element={<LevelOne />} />
-        <Route path="/level-two" element={<LevelTwo />} />
-        <Route path="/level-three" element={<LevelThree />} />
-      </Routes>
+      <AuthContextProvider>
+        {/* <SignIn /> */}
+        <Dropdown />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/level-one" element={<LevelOne />} />
+          <Route path="/level-two" element={<LevelTwo />} />
+          <Route path="/level-three" element={<LevelThree />} />
+        </Routes>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 };
